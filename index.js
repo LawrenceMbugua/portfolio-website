a061c460b3f866ee7d1b77ee97fe789685559e3e
Lawrence Mbugua <mbugualawrence87@gmail.com>
2022-11-13 11:54:29 +0300

    second commit

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 //get the form by its id
 const form = document.getElementById("contact-form"); 
 
-//1.
+
 const formEvent = form.addEventListener("submit", (event) => {
     event.preventDefault();
   
-    //2.
     let mail = new FormData(form);
-  
-    //3.
+
     sendMail(mail);
   })
 
   const sendMail = (mail) => {
-    //1.
+   
     fetch("/send", {
       method: 'POST',
       body: mail
